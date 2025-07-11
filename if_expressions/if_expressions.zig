e8bab82b3bf3b1c2d8f3db1df20e635e59810842
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-11 16:09:25 +0530

    Added if expressions

--- if_expressions/if_expressions.zig
+++ if_expressions/if_expressions.zig
@@ -12,3 +12,11 @@ test "if statement" {
 
     try expect(x == 1);
 }
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
